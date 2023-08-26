9ee5c79cbff888804c54fe93c0b3cfc5f8ce591e
pandadev666 <smartguy.panda666@gmail.com>
2023-08-26 02:43:28 -0700

    create dynamic destination table

--- integrations/postgres/main.go
+++ integrations/postgres/main.go
@@ -3,17 +3,56 @@ package postgres
 import (
 	"fmt"
 	"time"
+	"strings"
 	"database/sql"
 	"encoding/json"
 	"kassette.ai/kassette-server/utils/logger"
 )
 
-type HandleT struct {
-	DBHandle		*sql.DB			`json:"DBHandle"`
-	TableName		string			`json:"TableName`
+type SchemaFieldT struct {
+	Name			string				`json:"name"`
+	Type			string				`json:"type"`
+	PrimaryKey		bool				`json:"primary_key"`
 }
 
-func(handle *HandleT) Connect(config string) bool {
+type SchemaT struct {
+	TableName		string				`json:"table_name"`
+	SchemaFields	[]SchemaFieldT		`json:"schema_fields"`
+}
+
+type HandleT struct {
+	DBHandle		*sql.DB			`json:"DBHandle"`
+	Schema			SchemaT			`json:"Schema"`
+}
+
+func (handle *HandleT) createDestinationTable(schema string) bool {
+	err := json.Unmarshal([]byte(schema), &handle.Schema)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Error while creating destination table [%s]. Error: %s", schema, err.Error()))
+		return false
+	}
+	createTableSql := `CREATE TABLE IF NOT EXISTS %s (%s);`
+	fieldDefinitionList := []string{}
+	for _, field := range handle.Schema.SchemaFields {
+		fieldDefinition := field.Name + " " + field.Type
+		if field.Type == "VARCHAR" {
+			fieldDefinition +="(255)"
+		}
+		if field.PrimaryKey {
+			fieldDefinition += " PRIMARY KEY"
+		}
+		fieldDefinitionList = append(fieldDefinitionList, fieldDefinition)
+	}
+	sqlStatement := fmt.Sprintf(createTableSql, handle.Schema.TableName, strings.Join(fieldDefinitionList, ","))
+	_, err = handle.DBHandle.Exec(sqlStatement)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Error while creating destination table [%s]. Error: %s", schema, err.Error()))
+		return false
+	}
+	return true
+}
+
+func (handle *HandleT) Connect(config string) bool {
 	var configMap map[string]string
 	err := json.Unmarshal([]byte(config), &configMap)
 	if err != nil {
@@ -33,8 +72,21 @@ func(handle *HandleT) Connect(config string) bool {
 		logger.Error(fmt.Sprintf("Failed to ping [%s]. Error: %s", config, err.Error()))
 		return false
 	}
+
 	handle.DBHandle = dbHandle
-	handle.TableName = "camunda_eventlog"
+
+	schemaStr, ok := configMap["schema"]
+	if !ok {
+		logger.Error(fmt.Sprintf("Can not find schema in the destination config. [%s]", config))
+		return false
+	}
+
+	status := handle.createDestinationTable(schemaStr)
+
+	if !status {
+		return false;
+	}
+
 	logger.Info(fmt.Sprintf("Destination Connected [%s].", config))
 	return true
 }
@@ -45,7 +97,7 @@ func (handle *HandleT) InsertPayloadInTransaction(payloads []json.RawMessage) er
 		return err
 	}
 	for _, payload := range payloads {
-		insertStmt := fmt.Sprintf(`INSERT INTO %s (payload, received_at) VALUES ($1, $2)`, handle.TableName)
+		insertStmt := fmt.Sprintf(`INSERT INTO %s (payload, received_at) VALUES ($1, $2)`, handle.Schema.TableName)
 		_, err = tx.Exec(insertStmt, payload, time.Now())
 		if err != nil {
 			return err
