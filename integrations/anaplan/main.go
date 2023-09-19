5e2c0c1c0e3f14c63815fc00bc5de875572e5a35
pandadev666 <smartguy.panda666@gmail.com>
2023-09-18 19:56:19 -0700

    fix anaplan refresh token

--- integrations/anaplan/main.go
+++ integrations/anaplan/main.go
@@ -3,6 +3,7 @@ package anaplan
 import (
 	"io"
 	"fmt"
+	"time"
 	"bytes"
 	"encoding/json"
 	"net/url"
@@ -18,6 +19,7 @@ var (
 		"TEXT": 	 		"string",
 		"DATE":				"date",
 	}
+	tokenRefreshInterval = 20
 )
 
 type MetaT struct {
@@ -39,15 +41,16 @@ type AuthInfoT struct {
 }
 
 type HandleT struct {
-	AuthUrl			string				`json:"AuthUrl"`
-	UserName		string				`json:"UserName"`
-	Password		string				`json:"Password"`
-	Url				string				`json:"Url"`
-	Method			string				`json:"Method"`
-	Query			string				`json:"Query"`
-	Header			map[string]string	`json:"Header"`
-	AuthInfo		AuthInfoT
-	Client			*http.Client
+	AuthUrl				string				`json:"AuthUrl"`
+	UserName			string				`json:"UserName"`
+	Password			string				`json:"Password"`
+	RefreshTokenUrl 	string				`json:"RefreshTokenUrl"`
+	Url					string				`json:"Url"`
+	Method				string				`json:"Method"`
+	Query				string				`json:"Query"`
+	Header				map[string]string	`json:"Header"`
+	AuthInfo			AuthInfoT
+	Client				*http.Client
 }
 
 type FailureResponseT struct {
@@ -70,13 +73,23 @@ func (handle *HandleT) getFullUrl() string {
 	return handle.Url + "?" + handle.Query
 }
 
-func (handle *HandleT) authenticate() (bool, string) {
-	req, err := http.NewRequest("POST", handle.AuthUrl, nil)
-	if err != nil {
-		return false, err.Error()
+func (handle *HandleT) authenticate(isTokenRefresh bool) (bool, string) {
+	var req *http.Request
+	if isTokenRefresh {
+		tokenReq, err := http.NewRequest("POST", handle.RefreshTokenUrl, nil)
+		if err != nil {
+			return false, err.Error()
+		}
+		tokenReq.Header.Set("authorization", "AnaplanAuthToken " + handle.AuthInfo.TokenInfo.TokenValue)
+		req = tokenReq
+	} else {
+		authReq, err := http.NewRequest("POST", handle.AuthUrl, nil)
+		if err != nil {
+			return false, err.Error()
+		}
+		authReq.SetBasicAuth(handle.UserName, handle.Password)
+		req = authReq
 	}
-	req.SetBasicAuth(handle.UserName, handle.Password)
-
 	resp, err := handle.Client.Do(req)
 	if err != nil {
 		return false, err.Error()
@@ -95,6 +108,22 @@ func (handle *HandleT) authenticate() (bool, string) {
 		return true, ""
 	} else {
 		return false, handle.AuthInfo.StatusMessage
+	}
+}
+
+func (handle *HandleT) refreshAuthToken() (bool, string) {
+	ticker := time.NewTicker(time.Duration(tokenRefreshInterval) * time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <- ticker.C:
+			success, _ := handle.authenticate(true)
+			if success {
+				logger.Info("Anaplan Token Refresh Successful!")
+			} else {
+				logger.Error("Anaplan Token Refresh Failed!")
+			}
+		}
 	}
 }
 
@@ -126,6 +155,13 @@ func (handle *HandleT) Init(config string) bool {
 		return false
 	}
 	handle.Password = password.(string)
+
+	refreshTokenUrl, exist := configMap["refreshtokenurl"]
+	if !exist {
+		logger.Error("RefreshTokenUrl field does not exist in the config data.")
+		return false
+	}
+	handle.RefreshTokenUrl = refreshTokenUrl.(string)
 
 	destUrl, exist := configMap["url"]
 	if !exist {
@@ -174,10 +210,11 @@ func (handle *HandleT) Init(config string) bool {
 
 	handle.Client = &http.Client{}
 
-	authenticated, errMsg := handle.authenticate()
+	authenticated, errMsg := handle.authenticate(false)
 
 	if authenticated {
 		logger.Info(fmt.Sprintf("Rest Destination Connected! %v", *handle))
+		go handle.refreshAuthToken()
 		return true
 	} else {
 		logger.Info(fmt.Sprintf("Authentication Failed! %s", errMsg))
